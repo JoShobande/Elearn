b7bf7436bbab8ea4344536968f592681ea1a5364
Josephine <shobande.josephin@gmail.com>
2021-02-04 09:35:36 +0100

    firstCommit

---
+++ src/components/menu/index.js
@@ -0,0 +1,60 @@
+import React from "react"
+import homeMenuIcon from "../assets/images/homeMenuIcon.svg"
+import courseMenuIcon from "../assets/images/courseMenuIcon.svg"
+import favouriteMenuIcon from "../assets/images/favouriteMenuIcon.svg"
+import testMenuIcon from "../assets/images/testMenuIcon.svg"
+import achMenuIcon from "../assets/images/achMenuIcon.svg"
+import certMenuIcon from "../assets/images/certMenuIcon.svg"
+import settingsMenuIcon from "../assets/images/settingsMenuIcon.svg"
+import styles from './index.module.scss';
+
+
+const menuItems = [
+    {
+        icon: homeMenuIcon,
+        title: "Home"
+    },
+    {
+        icon: courseMenuIcon,
+        title: "My Courses"
+    },
+    {
+        icon: favouriteMenuIcon,
+        title: "Favourite"
+    },
+    {
+        icon: testMenuIcon,
+        title: "Test"
+    },
+    {
+        icon: achMenuIcon,
+        title: "Achievements"
+    },
+    {
+        icon: certMenuIcon,
+        title: "Certificates"
+    },
+    {
+        icon: settingsMenuIcon,
+        title: "settings"
+    }
+]
+
+const Menu = () =>{
+    return(
+        <div>
+            {
+                menuItems.map((menuItem)=>{
+                   return (
+                            <div className={styles.__menu_item}>
+                                <img src={menuItem.icon} alt="icon"/>
+                                <p>{menuItem.title}</p>
+                            </div>
+                          )
+                })
+            }
+        </div>
+    )
+}
+
+export default Menu;
---
+++ src/routes/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+import SideBar from "../components/sideBar"
+
+const Routes = () =>{
+    return(
+        <div>
+            <SideBar/>
+        </div>
+    )
+}
+
+export default Routes;
